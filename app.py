d42bccfbbbe65f3150be71d9c75381e382091226
Saurabh Tiwari <129385426+Rexaintreal@users.noreply.github.com>
2025-12-27 09:16:50 +0530

    Created routes for all the paradoxes and created global favicon for all the pages, updated paradox.json

--- app.py
+++ app.py
@@ -10,5 +10,29 @@ def index():
 def paradoxes():
     return render_template('paradoxes.html')
 
+@app.route('/paradox/monty-hall')
+def monty_hall():
+    return render_template('monty-hall.html')
+
+@app.route('/paradox/birthday')
+def birthday():
+    return render_template('birthday.html')
+
+@app.route('/paradox/braess')
+def braess():
+    return render_template('braess.html')
+
+# @app.route('/paradox/simpson')
+# def simpson():
+#     return render_template('simpson.html')
+
+# @app.route('/paradox/parrondo')
+# def parrondo():
+#     return render_template('parrondo.html')
+
+# @app.route('/paradox/prisoner')
+# def prisoner():
+#     return render_template('prisoner.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
